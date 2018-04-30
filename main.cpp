b09091270229bff5df6b9cba27bcbb83dae29851
DanielSoaresM <daniel.soares@mail.udp.cl>
2018-04-30 17:28:40 -0300

    se ordeno el codigo

--- main.cpp
+++ main.cpp
@@ -55,17 +55,15 @@ lineas* cargarmapa(string archivo,int* num){
 }
 
 
-void ctuberias(int linact, int (&direcion)[4]){
+void cpostuberias(int linact, int (&direcion)[4]){
 	//dicionario para lineas actuales
-	int dic[9]={0,1,0,3,1,5,2,7,3};
-	int arr[4][4]={1,0,0,-1,0,1,-1,0,1,0,-1,0,0,1,0,-1};
+	int dic[9]={-1,-1,0,-1,1,-1,2,-1,3};//dicionario de lineas
+	int arr[4][4]={1,0,0,-1,0,1,-1,0,1,0,-1,0,0,1,0,-1};//rotaciones por palanca
 	cout<<"mover llave"<<endl;
 	for(int i=0;i<4;i++){
 		cout<<direcion[i]<<" ";
 	}
 	cout<<endl;
-	
-
 	while(1){
 		if(key[KEY_D]){
 			for(int i=0;i<4;i++){
@@ -85,6 +83,14 @@ void ctuberias(int linact, int (&direcion)[4]){
 			break;
 		}
 	}
+	for (int i = 0; i < 4; i++){
+		if (*(direcion+i)==4){
+			*(direcion+i)=0;
+		}
+		if (*(direcion+i)==-1){
+			*(direcion+i)=3;
+		}
+	}
 }
 
 
@@ -97,57 +103,32 @@ bool jugarnivel1(){
 	lineas* ruta = cargarmapa(archivo, n);
 	cout<<"se cargo en mapa"<<endl;
 	//cargaresp("1E.txt",)
-	float progreso=1;
-	int lineact=0;
-	int colesp = makecol(255,94,0);
-	int colcamino = makecol(51,153,51);
-	int coljugador = makecol(255,255,0);
-	BITMAP *pantalla = create_bitmap(640,480);
+	float progreso=1;// progreso en porsentaje del jugador en la linea
+	int lineact=0;//linea en la que se encuentra el jugador
+	cords jugador;//cordenadas jugador
+	int postuberias[4]={0,3,0,2};//posisiones iniciales de tuberias
+
+	int colesp = makecol(255,94,0);//color camino especial
+	int colcamino = makecol(51,153,51);//color camino normal
+	int coljugador = makecol(255,255,0);//color jugador
+		
+
+	BITMAP *pantalla = create_bitmap(640,480); //iniciar bitmaps
 	BITMAP *tuberia = create_bitmap(20,80);
-	line(tuberia,10,20,10,60,colcamino);
+	clear_bitmap(pantalla);
+	clear_bitmap(tuberia);
+
+	line(tuberia,10,20,10,60,colcamino);//dibujar tuberias probicionales
 	circle(tuberia,10,60,7,coljugador);
-	clear_bitmap(pantalla);
+
 	
-	for(int i=0;i<*n;i++)
-		line(pantalla,(*(ruta+i)).ini.x,480-(*(ruta+i)).ini.y,(*(ruta+i)).fin.x,480-(*(ruta+i)).fin.y,colcamino);
-
-	cords jugador;
-	jugador.x=(int)ruta[lineact].ini.x+(ruta[lineact].fin.x-ruta[lineact].ini.x)*progreso/100 ;
-	jugador.y=(int)ruta[lineact].ini.y+(ruta[lineact].fin.y-ruta[lineact].ini.y)*progreso/100 ;
-	circle(pantalla,jugador.x,480-jugador.y,10,coljugador);
-	blit(pantalla,screen,0,0,0,0,640,480);
-	clear_bitmap(pantalla);
-
-
-	int tuberias[4]={0,3,0,2};
 	
 
 	while(!key[KEY_ESC]){
-		//movimiento
-		readkey();
-		if(key[KEY_D])
-			progreso=progreso+(*(ruta+lineact)).progreso;
 
-		if(key[KEY_A])
-			progreso=progreso-ruta[lineact].progreso;
-
-		if(ruta[lineact].cond==1 && key[KEY_W])
-			ctuberias(lineact,tuberias);
-
-
-		if(progreso>=100){
-			progreso=1;
-			lineact=ruta[lineact].lins;
-		}
-
-		if(progreso<=0){
-			progreso=99;
-			lineact=ruta[lineact].lina;
-		}
-
-		//imprimir y weas 
+		//inicio impreprecion 
 		for(int i=0;i<4;i++){
-			rotate_sprite(pantalla,tuberia,100+150*i,150,itofix(tuberias[i]*64));
+			rotate_sprite(pantalla,tuberia,100+150*i,150,itofix(postuberias[i]*64));
 		}
 
 		for(int i=0;i<*n;i++){
@@ -162,15 +143,38 @@ bool jugarnivel1(){
 		circle(pantalla,jugador.x,480-jugador.y,10,coljugador);
 		blit(pantalla,screen,0,0,0,0,640,480);
 		clear_bitmap(pantalla);
+		//fin inmprecion
+
+		//inicio movimiento
+		readkey();
+		if(key[KEY_D])
+			progreso=progreso+(*(ruta+lineact)).progreso;
+
+		if(key[KEY_A])
+			progreso=progreso-ruta[lineact].progreso;
+
+		if(ruta[lineact].cond==1 && key[KEY_W])
+			cpostuberias(lineact,postuberias);
+
+		if(progreso>=100){
+			progreso=1;
+			lineact=ruta[lineact].lins;
+		}
+
+		if(progreso<=0){
+			progreso=99;
+			lineact=ruta[lineact].lina;
+		}
+		//fin movimiento
 	}
 	return 0;
 }
 
 
 int main (){
-	//cargar el mapa
+	//inicializar weas de allegro
 	allegro_init();
-	set_window_title("Hello World");
+	set_window_title("CandyLand");
 	install_keyboard();
 	set_gfx_mode(GFX_AUTODETECT_WINDOWED, 640, 480, 0, 0);
 	
