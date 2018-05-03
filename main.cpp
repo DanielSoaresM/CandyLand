34b4145013bf61119e89378da3bf40cd35e2f00a
DanielSoaresM <daniel.soares@mail.udp.cl>
2018-05-02 21:47:18 -0300

    nivel 2 beta

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ struct lineas{
 lineas* cargarmapa(string archivo,int* num){
 	int n;
 	fstream rutas;
-	rutas.open("1m.txt", fstream::in);
+	rutas.open(archivo.c_str(), fstream::in);
 	cout<<"se abrio archivo"<<endl;
 	rutas >> n;
 	cout<<n<<endl;
@@ -108,7 +108,7 @@ bool jugarnivel1(){
 	cords jugador;//cordenadas jugador
 	int postuberias[4]={0,3,0,2};//posisiones iniciales de tuberias
 
-	int colesp = makecol(255,94,0);//color camino especial
+	int colpalanca = makecol(255,94,0);//color camino especial
 	int colcamino = makecol(51,153,51);//color camino normal
 	int coljugador = makecol(255,255,0);//color jugador
 		
@@ -133,7 +133,7 @@ bool jugarnivel1(){
 
 		for(int i=0;i<*n;i++){
 			if ((*(ruta+i)).cond)
-				line(pantalla,(*(ruta+i)).ini.x,480-(*(ruta+i)).ini.y,(*(ruta+i)).fin.x,480-(*(ruta+i)).fin.y,colesp);
+				line(pantalla,(*(ruta+i)).ini.x,480-(*(ruta+i)).ini.y,(*(ruta+i)).fin.x,480-(*(ruta+i)).fin.y,colpalanca);
 			else
 				line(pantalla,(*(ruta+i)).ini.x,480-(*(ruta+i)).ini.y,(*(ruta+i)).fin.x,480-(*(ruta+i)).fin.y,colcamino);
 		}
@@ -170,6 +170,77 @@ bool jugarnivel1(){
 	return 0;
 }
 
+bool jugarnivel2(){
+	string archivo="2m.txt";
+	int * n=new int;
+	*n =5;
+	cout<<*n<<endl;
+	lineas* ruta = cargarmapa(archivo, n);
+	cout<<"se cargo en mapa"<<endl;
+	//cargaresp("1E.txt",)
+	float progreso=1;// progreso en porsentaje del jugador en la linea
+	int lineact=0;//linea en la que se encuentra el jugador
+	cords jugador;//cordenadas jugador
+
+	int colpalanca = makecol(204,0,204);//color camino especial
+	int colcamino = makecol(0,0,204);//azul
+	int coljugador = makecol(0,255,0);//verde
+	int colescaleras = makecol(255,255,51);//amarillo		
+
+	BITMAP *pantalla = create_bitmap(640,480); //iniciar bitmaps
+	BITMAP *tuberia = create_bitmap(20,80);
+	clear_bitmap(pantalla);
+
+
+	while(!key[KEY_ESC]){
+
+		//inicio impreprecion 
+		for(int i=0;i<*n;i++){
+			if ((*(ruta+i)).cond==1)
+				line(pantalla,(*(ruta+i)).ini.x,480-(*(ruta+i)).ini.y,(*(ruta+i)).fin.x,480-(*(ruta+i)).fin.y,colpalanca);
+			else{
+				if ((*(ruta+i)).cond==2)
+					line(pantalla,(*(ruta+i)).ini.x,480-(*(ruta+i)).ini.y,(*(ruta+i)).fin.x,480-(*(ruta+i)).fin.y,colescaleras);	
+				else
+					line(pantalla,(*(ruta+i)).ini.x,480-(*(ruta+i)).ini.y,(*(ruta+i)).fin.x,480-(*(ruta+i)).fin.y,colcamino);
+			}
+		}
+		
+		jugador.x=(int)ruta[lineact].ini.x+(ruta[lineact].fin.x-ruta[lineact].ini.x)*progreso/100 ;
+		jugador.y=(int)ruta[lineact].ini.y+(ruta[lineact].fin.y-ruta[lineact].ini.y)*progreso/100 ;
+		circle(pantalla,jugador.x,480-jugador.y,10,coljugador);
+		blit(pantalla,screen,0,0,0,0,640,480);
+		clear_bitmap(pantalla);
+		//fin inmprecion
+
+		//inicio movimiento
+		readkey();
+		if(key[KEY_D])
+			progreso=progreso+(*(ruta+lineact)).progreso;
+
+		if(key[KEY_A])
+			progreso=progreso-ruta[lineact].progreso;
+
+		if(progreso>=100){
+			progreso=1;
+			lineact=ruta[lineact].lins;
+		}
+
+		if(progreso<=0){
+			progreso=99;
+			lineact=ruta[lineact].lina;
+		}
+		//fin movimiento
+	}
+	return 0;
+}
+
+
+
+
+
+
+
 
 int main (){
 	//inicializar weas de allegro
@@ -178,6 +249,6 @@ int main (){
 	install_keyboard();
 	set_gfx_mode(GFX_AUTODETECT_WINDOWED, 640, 480, 0, 0);
 	
-	jugarnivel1();
+	jugarnivel2();
 	allegro_exit();
 }END_OF_MAIN()
